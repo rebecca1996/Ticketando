a1017b0f8ba095da25488a9a0097fdfa445e778d
Rebecca <rebecca.ignacio@etec.sp.gov.br>
2018-03-16 21:32:06 -0300

    Add files via upload

---
+++ ProjetoInterdisciplinar/Models/ParticiapanteComum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoInterdisciplinar.Models
+{
+    class ParticiapanteComum : Participante
+    {
+        public int RA { get; set; }
+    }
+}
---
+++ ProjetoInterdisciplinar/Models/Participante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoInterdisciplinar.Models
+{
+    class Participante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+        public string rg { get; set; }
+    }
+}
